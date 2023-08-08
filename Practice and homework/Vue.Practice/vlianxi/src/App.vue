b13240b08ec13a6507056dca3979ceaf89a558ca
zhao <3556526173@qq.com>
2023-08-08 17:21:17 +0800

    提交

---
+++ Practice and homework/Vue.Practice/vlianxi/src/App.vue
@@ -0,0 +1,20 @@
+<template>  
+  <div id="app">  
+    <refDemo :getNameFun="getName"  />  
+  </div>  
+</template>  
+  
+<script>  
+import refDemo from './components/StudentClass.vue';   
+  
+export default {  
+  components: {  
+    refDemo  
+  },  
+  methods:{  
+    getName(name){  
+      console.log(name);  
+    }  
+  }  
+}  
+</script>  
